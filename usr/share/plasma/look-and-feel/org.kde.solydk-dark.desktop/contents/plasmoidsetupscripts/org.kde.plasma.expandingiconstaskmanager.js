ee1dc934b8c4483b79e2e92bc6555d425c127a25
abalfoort <arjenbalfoort@hotmail.com>
2023-02-20 16:25:31 +0100

    Remove redundant plasmoidsetupscripts

--- usr/share/plasma/look-and-feel/org.kde.solydk-dark.desktop/contents/plasmoidsetupscripts/org.kde.plasma.expandingiconstaskmanager.js
+++
@@ -1,3 +0,0 @@
-applet.currentConfigGroup = ["Configuration", "General"]
-applet.writeConfig("launchers" ,"file:///usr/share/applications/org.kde.dolphin.desktop?wmClass=dolphin,file:///usr/share/applications/firefox-esr.desktop?wmClass=Firefox")
-applet.writeConfig("showOnlyCurrentDesktop", true)
